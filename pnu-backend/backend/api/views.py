557f5c1cf88e7cda9b86c517fa3773e24f5d1b6b
ChoMinGi <81455273+ChoMinGi@users.noreply.github.com>
2022-09-17 11:53:39 +0900

    Provide test module
    GET, POST test module

--- pnu-backend/backend/api/views.py
+++ pnu-backend/backend/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework.response import Response
 from django.http import JsonResponse
 from .serializer import MyTokenObtainPairSerializer, RegisterSerializer
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny, IsAuthenticated
-
+from rest_framework.decorators import api_view, permission_classes
 # Create your views here.
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -27,3 +27,16 @@ def getRoutes(request):
         '/api/token/refresh/'
     ]
     return Response(routes)
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+
+def testEndPoint(request):
+    if request.method == 'GET':
+        data = f"Congratulation {request.user}, your API just responded to GET request"
+        return Response({'response': data}, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        text = request.POST.get('text')
+        data = f'Congratulation your API just responded to POST request with text: {text}'
+        return Response({'response': data}, status=status.HTTP_200_OK)
+    return Response({}, status.HTTP_400_BAD_REQUEST)
