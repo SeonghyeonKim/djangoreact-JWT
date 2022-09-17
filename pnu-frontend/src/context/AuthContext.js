3ee436e68ef82489f11d00127074473adc2265dd
ChoMinGi <81455273+ChoMinGi@users.noreply.github.com>
2022-09-17 15:43:54 +0900

    Update AuthContext.js
    Add remark

--- pnu-frontend/src/context/AuthContext.js
+++ pnu-frontend/src/context/AuthContext.js
@@ -6,7 +6,7 @@ const AuthContext = createContext();
 
 export default AuthContext;
 
-export const AuthProvider = ({ children }) => {
+export const AuthProvider =({ children }) => {
   const [authTokens, setAuthTokens] = useState(() =>
     localStorage.getItem("authTokens")
       ? JSON.parse(localStorage.getItem("authTokens"))
@@ -21,6 +21,7 @@ export const AuthProvider = ({ children }) => {
 
   const history = useHistory();
 
+  // username과 pw 가 DB내에 존재하게 되면 로그인이 된다. 토큰은 로컬 저장소에 저장됨
   const loginUser = async (username, password) => {
     const response = await fetch("http://127.0.0.1:8000/api/token/", {
       method: "POST",
@@ -40,11 +41,12 @@ export const AuthProvider = ({ children }) => {
       localStorage.setItem("authTokens", JSON.stringify(data));
       history.push("/");
     } else {
-      alert("Something went wrong!");
+      alert("응답코드가 200이 아닙니다. 로컬에 토큰 저장이 불가합니다.");
     }
   };
 
-  const registerUser = async (username, password, password2) => {
+  // 가입시에 username 과 pw, pwcheck 입력시 응답이 성공적으로 이루어지면 login 페이지로 리다이렉트
+  const registerUser = async (username, password, passwordcheck) => {
     const response = await fetch("http://127.0.0.1:8000/api/register/", {
       method: "POST",
       headers: {
@@ -53,13 +55,13 @@ export const AuthProvider = ({ children }) => {
       body: JSON.stringify({
         username,
         password,
-        password2
+        passwordcheck
       })
     });
     if (response.status === 201) {
       history.push("/login");
     } else {
-      alert("Something went wrong!");
+      alert("응답코드가 200이 아닙니다. login 페이지로의 전환이 불가합니다.");
     }
   };
 
