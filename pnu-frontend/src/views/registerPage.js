7ae4c89fb0a56b27176c34bbd7e93d9297b3a8a2
ChoMinGi <81455273+ChoMinGi@users.noreply.github.com>
2022-09-17 15:56:22 +0900

    Update views
    With Opensource

---
+++ pnu-frontend/src/views/registerPage.js
@@ -0,0 +1,57 @@
+import { useState, useContext } from "react";
+import AuthContext from "../context/AuthContext";
+
+function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordcheck, setPasswordcheck] = useState("");
+  const { registerUser } = useContext(AuthContext);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    registerUser(username, password, passwordcheck);
+  };
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        <hr />
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            onChange={e => setUsername(e.target.value)}
+            placeholder="Username"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            onChange={e => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input
+            type="password"
+            id="confirm-password"
+            onChange={e => setPasswordcheck(e.target.value)}
+            placeholder="Confirm Password"
+            required
+          />
+          <p>{passwordcheck !== password ? "Passwords do not match" : ""}</p>
+        </div>
+        <button>Register</button>
+      </form>
+    </section>
+  );
+}
+
+export default Register;
