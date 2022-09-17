813559cca9e84e7dd0fe8ead7a3190f7c546a7f0
ChoMinGi <81455273+ChoMinGi@users.noreply.github.com>
2022-09-17 20:27:18 +0900

    Fix Syntax error

--- pnu-frontend/src/views/homePage.js
+++ pnu-frontend/src/views/homePage.js
@@ -9,7 +9,7 @@ const Home = () => {
     <section>
       {user && <UserInfo user={user} />}
       <h1>이 윗쪽의 section 은 Navbar 형태로 구현하였습니다. (components/Navbar)</h1>
-      <h1>메인 페이지입니다. 아직 로그인을 하지 않은 상태입니다.</h1>
+      <h1>메인 페이지입니다.</h1>
     </section>
   );
 };
